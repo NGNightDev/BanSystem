01eea53b5f80b56dd014da70b285821e4817312d
NGNightDev <sb10082003>
2018-08-20 22:29:57 +0200

    Uploaded first time.

--- src/main/java/net/hubgame/bansystem/Main.java
+++ src/main/java/net/hubgame/bansystem/Main.java
@@ -1,7 +1,29 @@
 package net.hubgame.bansystem;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("d");
+import lombok.Getter;
+import net.hubgame.bansystem.mysql.MySQLBans;
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Main extends Plugin {
+
+    public static String PREFIX = "§8»§e Ban §8×§7 ";
+    public static String DISCONNECTPREFIX = "§7➤ §bHubGame.net ";
+
+    @Getter
+    private static Main instance;
+    @Getter
+    private MySQLBans mySQLBans;
+
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        mySQLBans = new MySQLBans();
     }
+
+    @Override
+    public void onDisable() {
+
+    }
+
 }
